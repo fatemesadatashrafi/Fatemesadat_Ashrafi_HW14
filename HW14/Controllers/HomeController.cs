5642408c7296cbccbaa999d4a1d46c6a88909655
fatemesadat ashrafi <fatemesadat.ashrafi@yahoo.com>
2024-08-02 13:45:25 +0330

    final commit

--- HW14/Controllers/HomeController.cs
+++ HW14/Controllers/HomeController.cs
@@ -1,13 +1,18 @@
 using HW14.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
 using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace HW14.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private static string FilePath { get; set; } = "UserInformation.txt";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -27,18 +32,66 @@ namespace HW14.Controllers
         {
             return View();
         }
-        public IActionResult Submit(UserInformationViewModel u, bool acceptingRules)
+        [HttpPost]
+        public IActionResult Submit(UserInformationViewModel u)
         {
 
             if (ModelState.IsValid)
-            { }
-            return Json(new { acceptingRules = acceptingRules});
+            {
+                if (u.AcceptingRules==false)
+                {
+                    ViewBag.AcceptingRules = "false";
+                    return View(nameof(Subscription));
+                }
+                else
+                {
+                    Validation(u);
+                    if (ViewBag.FirstNameType == "false" || ViewBag.LastNameType == "false" || ViewBag.BootcampCode == "false" || ViewBag.Age == "false")
+                    {
+                        return View(nameof(Subscription));
+                    }
+                    else
+                    {
+                        var json = JsonConvert.SerializeObject(u, Formatting.Indented);
+                        string jsonString = JsonConvert.SerializeObject(json);
+                        System.IO.File.WriteAllText(FilePath, jsonString);
+                        return Json(u);
+                    }
+                }
+            }
+            //return Json(new { acceptingRules = acceptingRules});
+            return View(nameof(Subscription));
+        }
+        public IActionResult Rules()
+        {
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public void Validation(UserInformationViewModel u)
+        {
+            if (!Regex.IsMatch(u.FirstName.Trim(), @"^[a-zA-Z]+$"))
+            
+                ViewBag.FirstNameType = "false";
+            else
+                ViewBag.FirstNameType = "true";
+
+            if (!Regex.IsMatch(u.LastName.Trim(), @"^[a-zA-Z]+$"))
+                ViewBag.LastNameType = "false";
+            else
+                ViewBag.LastNameType = "true";
+            if (!Regex.IsMatch(u.BootcampCode.ToString(), @"^[2468]\d{2}$"))
+                ViewBag.BootcampCode = "false";
+            else
+                ViewBag.BootcampCode = "true";
+            if ((DateTime.Now - u.BirthDate).TotalDays < 6570)
+                ViewBag.Age = "false";
+            else
+                ViewBag.Age = "true";
         }
     }
 }
--- HW14/Models/UserInformationViewModel.cs
+++ HW14/Models/UserInformationViewModel.cs
@@ -1,13 +1,31 @@
-﻿namespace HW14.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace HW14.Models
 {
     public class UserInformationViewModel
     {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public int PhoneNumber { get; set; }
-        public DateOnly BirthDate { get; set; }
-        public int BootcampCode { get; set; }
+        [Required(ErrorMessage = "وارد کردن نام الزامی می باشد.")]
+        [MinLength(3, ErrorMessage = "طول نام نمی تواند کمتر از سه کاراکتر باشد.")]
+        [MaxLength(30, ErrorMessage = "طول نام نمی تواند بیشتر از سی کاراکتر باشد.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "وارد کردن نام خانوادگی الزامی می باشد.")]
+        [MinLength(3, ErrorMessage = "طول نام خانوادگی نمی تواند کمتر از سه کاراکتر باشد.")]
+        [MaxLength(30, ErrorMessage = "طول نام خانوادگی نمی تواند بیشتر از سی کاراکتر باشد.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "وارد کردن شماره همراه الزامی می باشد.")]
+        [RegularExpression(@"^09\d{9}", ErrorMessage = "شماره را اشتباه وارد کرده اید.")]
+        public string? PhoneNumber { get; set; }
+        [Required(ErrorMessage = "وارد کردن تاریخ تولد الزامی می باشد.")]
+        public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage = "وارد کردن کد دوره الزامی می باشد.")]
+        [RegularExpression(@"^[2468]\d{2}$", ErrorMessage = "کد دوره را اشتباه وارد کرده اید.")]
+        public int? BootcampCode { get; set; }
+        [EnumDataType(typeof(Gender), ErrorMessage = "انتخاب جنسیت الزامی می باشد.")]
         public Gender Gender { get; set; }
+        //[Range(typeof(bool), "true", "true", ErrorMessage = "قوانین سایت رو نمی پذیری؟")]
+        [JsonIgnore]
+        public bool AcceptingRules { get; set; }
 
     }
     public enum Gender
